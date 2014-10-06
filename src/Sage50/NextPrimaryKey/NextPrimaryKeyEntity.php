82b4096e8053482720298e1f9c8aa84803c05575
Gabriel Bull <me@gabrielbull.com>
2014-10-06 19:51:05 -0400

    Added next primary key entity

---
+++ src/Sage50/NextPrimaryKey/NextPrimaryKeyEntity.php
@@ -0,0 +1,174 @@
+<?php
+namespace Sinergi\Sage50\NextPrimaryKey;
+
+use DateTime;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="tnxtpids")
+ */
+class NextPrimaryKeyEntity
+{
+    const SALE_ORDER_ID = 62;
+
+    /**
+     * @ORM\Id
+     * @ORM\Column(name="lId", type="integer")
+     * @ORM\GeneratedValue
+     * @var int
+     */
+    private $id = 0;
+
+    /**
+     * @ORM\Column(name="dtASDate", type="datetime", nullable=true)
+     * @var DateTime
+     */
+    private $modificationDate;
+
+    /**
+     * @ORM\Column(name="tmASTime", type="datetime", nullable=true)
+     * @var DateTime
+     */
+    private $modificationTime;
+
+    /**
+     * @ORM\Column(name="sASUserId", type="string", length=14, nullable=true)
+     * @var string
+     */
+    private $createdByUsername = 'sysadmin';
+
+    /**
+     * @ORM\Column(name="sASOrgId", type="string", length=6, nullable=true)
+     * @var string
+     */
+    private $createdByOrg = 'winsim';
+
+    /**
+     * @ORM\Column(name="lNextId", type="integer", nullable=true)
+     * @var int
+     */
+    private $nextId;
+
+    public function __construct()
+    {
+        $this->setModificationDate(new DateTime(date('Y-m-d 00:00:00')));
+        $this->setModificationTime(new DateTime(date('1899-12-30 H:i:s')));
+    }
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     * @return $this
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * @return DateTime
+     */
+    public function getModificationDate()
+    {
+        return $this->modificationDate;
+    }
+
+    /**
+     * @param DateTime $modificationDate
+     * @return $this
+     */
+    public function setModificationDate(DateTime $modificationDate)
+    {
+        $this->modificationDate = $modificationDate;
+        return $this;
+    }
+
+    /**
+     * @return DateTime
+     */
+    public function getModificationTime()
+    {
+        return $this->modificationTime;
+    }
+
+    /**
+     * @param DateTime $modificationTime
+     * @return $this
+     */
+    public function setModificationTime(DateTime $modificationTime)
+    {
+        $this->modificationTime = $modificationTime;
+        return $this;
+    }
+
+    /**
+     * @return DateTime
+     */
+    public function getModificationDatetime()
+    {
+        return new DateTime($this->modificationDate->format('Y-m-d') . ' ' . $this->modificationTime->format('H:i:s'));
+    }
+
+    /**
+     * @return string
+     */
+    public function getCreatedByUsername()
+    {
+        return $this->createdByUsername;
+    }
+
+    /**
+     * @param string $createdByUsername
+     * @return $this
+     */
+    public function setCreatedByUsername($createdByUsername)
+    {
+        $this->createdByUsername = $createdByUsername;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCreatedByOrg()
+    {
+        return $this->createdByOrg;
+    }
+
+    /**
+     * @param string $createdByOrg
+     * @return $this
+     */
+    public function setCreatedByOrg($createdByOrg)
+    {
+        $this->createdByOrg = $createdByOrg;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getNextId()
+    {
+        return $this->nextId;
+    }
+
+    /**
+     * @param int $nextId
+     * @return $this
+     */
+    public function setNextId($nextId)
+    {
+        $this->nextId = $nextId;
+        return $this;
+    }
+}
