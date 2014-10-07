3c681829511ed77e628c6af27cf236461e3a14dc
Gabriel Bull <me@gabrielbull.com>
2014-10-06 21:02:16 -0400

    Started work on Sale Order builder

---
+++ src/Sage50/SaleOrder/SaleOrderBuilder.php
@@ -0,0 +1,55 @@
+<?php
+namespace Sinergi\Sage50\SaleOrder;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Sinergi\Sage50\NextPrimaryKey\NextPrimaryKeyRepository;
+use Sinergi\Sage50\JournalEntry\JournalEntryRepository;
+
+class SaleOrderBuilder
+{
+    /**
+     * @var EntityManagerInterface
+     */
+    protected $entityManager;
+
+    /**
+     * @var SaleOrderEntity
+     */
+    private $saleOrder;
+
+    /**
+     * @var NextPrimaryKeyRepository
+     */
+    private $nextPrimaryKeyRepository;
+
+    /**
+     * @var JournalEntryRepository
+     */
+    private $journalEntryRepository;
+
+    /**
+     * @param EntityManagerInterface $entityManager
+     */
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+        $this->nextPrimaryKeyRepository = $this->entityManager->getRepository(
+            'Sinergi\\Sage50\\NextPrimaryKey\\NextPrimaryKeyEntity'
+        );
+        $this->journalEntryRepository = $this->entityManager->getRepository(
+            'Sinergi\\Sage50\\JournalEntry\\JournalEntryEntity'
+        );
+    }
+
+    public function createSaleOrder(SaleOrderEntity $saleOrder)
+    {
+        $this->addSaleOrder($saleOrder);
+    }
+
+    protected function addSaleOrder(SaleOrderEntity $saleOrder)
+    {
+        $this->saleOrder = $saleOrder;
+        $this->saleOrder->setId($this->nextPrimaryKeyRepository->fetchNextSaleOrderId());
+        $this->saleOrder->setNextId($this->journalEntryRepository->fetchNextJournalEntryId());
+    }
+}
